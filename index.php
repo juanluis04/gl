b4f3e91e3960a8a15a048d9621a1d8f4de638884
Luis Peralta <juanlperaltta@gmail.com>
2023-06-25 22:51:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 
 		// Define database connection variables
